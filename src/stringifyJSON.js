463509e0069854c0d9f180f8f2166eb4eff8c696
Kevin Coyner <kevin@rustybear.com>
2017-05-17 17:09:32 -0400

    doodle: Half way complete stringifyJSON.js.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,41 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var result = [];
+    for (let i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    var result = {};
+    for (var key in obj) {
+      result[key] = stringifyJSON([key]);
+      result[obj[key]] = stringifyJSON(obj[key]);
+      console.log('result[key]: ', result[key]);
+      console.log('result[obj[key]]: ', result[obj[key]]);
+    }
+    return '{' + result + '}';
+  }
+
+  return '' + obj;
+
 };
+
+var val = {'a': 'apple'};
+console.log(stringifyJSON(val));
+console.log(JSON.stringify(val));
