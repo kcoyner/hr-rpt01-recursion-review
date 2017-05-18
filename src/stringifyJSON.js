c70b34d582a2d356e3b74854de5d39557f6b2af1
Kevin Coyner <kevin@rustybear.com>
2017-05-17 23:55:35 -0400

    doodle: Progress capture Wed evening Steve and Kevin. Almost complete.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,54 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+//
+
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)){
+    var result = [];
+    for (var i = 0; i < obj.length; i++){
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result + ']';
+  }
+
+  if (typeof obj === 'object'){
+      if (obj === null){
+        return 'null';
+      }
+    if (typeof obj === 'function' || typeof obj === undefined){
+      return '';
+    }
+
+      // if (!obj){
+      // return 'undefined';
+
+    if (Object.keys(obj).length === 0){
+      return '{}';
+    } else {
+      var result = "";
+      for (var key in obj){
+        if (typeof key === 'function' || typeof obj[key]=== 'function'){
+          return '';
+        }
+      result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','
+    }
+      result = result.slice(0, -1);
+      return '{' + result + '}';
+    }
+  }
+
+  return '' + obj;
+
+
 };
+
+var test = function(){};
+console.log(stringifyJSON(test));
+console.log(JSON.stringify(test));
+
+
